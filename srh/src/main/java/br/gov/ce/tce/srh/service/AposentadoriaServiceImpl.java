ebc5c094b9ed7aeef1db0b32373e341605232a3b
TCE\felipe.augusto <felipe.augusto@tce.ce.gov.br>
2017-10-13 09:44:04 -0300

    #1865
    Validaçoes:
    
    1-Ano da Resolução tem que ser maior ou igual ao ano do Ato de Aposentadoria
    2-Data da publicação do ato deve ser maior ou igual à data do ato de aposentadoria
    3-Servidores aposentados não ficam disponíveis para exoneração
    4-Coluna [Ponto] da Tabela TB_FUNCIONAL deve assumior valor 0 (zero)
    5-Coluna [AbonoPrevidenciario] da Tabela TB_FUNCIONAL deve assumior valor 0 (zero)
    6-Deverá ser gerado um "Alerta" caso o servidor possua uma entrada ativa na Tabela TB_REPRESENTACAOFUNCIONAL.
    "Você está aposentando [nome do funcionário] e verificamos que o mesmo ainda está cadastrado como ocupante do cargo comissionado [xxxxxxxx]. Caso o servidor continue no cargo comissionado, favor desconsiderar essa mensagem, do contrário, favor gerar a exoneração do cargo comissionado do mesmo."
    7-Verificar se o último registro do servidor na Tabela TB_FUNCIONALSETOR já está com o IDSETOR=113, caso contrário, gerar um novo registro com a DATA_INICIO igual a o próximo dia após a última contagem e efetuar a baixa no registro do último setor ocupado pelo servidor. Lembrar de atualizar também o campo IDSETOR da Tabela TB_FUNCIONAL para 113.
    8-Na exclusão de um lançamento de aposentadoria, verificar se o servidor possuia dados na Tabela TB_ABONOPERMANENCIA, e, se afirmativo, gravar [1] no campo ABONOPREVIDENCIARIO da Tabela TB_FUNCIONAL.
    9-Ao excluir um lançamento de aposentadoria, deve-se retornar o valor do campo PONTO da Tabela TB_FUNCIONAL para [1], com exceção dos Conselheiros, Auditores e Procuradores.
    "Você está desfazendo um lançamento de aposentadoria, lembre-se de verificar se o servidor está obrigado a bater ponto."

--- srh/src/main/java/br/gov/ce/tce/srh/service/AposentadoriaServiceImpl.java
+++ srh/src/main/java/br/gov/ce/tce/srh/service/AposentadoriaServiceImpl.java
@@ -10,10 +10,13 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import br.gov.ce.tce.srh.dao.AposentadoriaDAO;
+import br.gov.ce.tce.srh.domain.AbonoPermanencia;
 import br.gov.ce.tce.srh.domain.Aposentadoria;
 import br.gov.ce.tce.srh.domain.Funcional;
+import br.gov.ce.tce.srh.domain.FuncionalSetor;
 import br.gov.ce.tce.srh.enums.EnumStatusFuncional;
 import br.gov.ce.tce.srh.exception.SRHRuntimeException;
+import br.gov.ce.tce.srh.sapjava.service.SetorService;
 
 @Service("aposentadoriaService")
 public class AposentadoriaServiceImpl implements AposentadoriaService {
@@ -27,23 +30,30 @@ public class AposentadoriaServiceImpl implements AposentadoriaService {
 	@Autowired
 	private FolhaService folhaService;
 	
+	@Autowired
+	private FuncionalSetorService funcionalSetorService;
+	
+	@Autowired
+	private SetorService setorService;
+	
+	@Autowired
+	private AbonoPermanenciaService abonoPermanenciaService;
+	
+	@Autowired
+	private RepresentacaoFuncionalService representacaoFuncionalService;
+	
 		
 	@Override
 	@Transactional
 	public Aposentadoria salvar(Aposentadoria entidade) throws SRHRuntimeException {
 		
-		
 		validaDadosObrigatorios(entidade);
 		
-		verificaAposentadoriaCadastrada(entidade);
-		
-		Funcional funcional = funcionalService.getById(entidade.getFuncional().getId());			
-		
-		verificaTipoOcupacao(funcional);
+		verificaAposentadoriaCadastrada(entidade);		
 		
 		entidade = dao.salvar(entidade);		
 		
-		atualizaFuncional(entidade, funcional, false);
+		atualizaFuncional(entidade, false);
 		
 	    
 		return entidade;
@@ -54,9 +64,7 @@ public class AposentadoriaServiceImpl implements AposentadoriaService {
 	@Transactional
 	public void excluir(Aposentadoria entidade) {
 		
-		Funcional funcional = funcionalService.getById(entidade.getFuncional().getId());
-		
-		atualizaFuncional(entidade, funcional, true);		
+		atualizaFuncional(entidade, true);		
 		
 		dao.excluir(entidade);		
 	}
@@ -78,13 +86,15 @@ public class AposentadoriaServiceImpl implements AposentadoriaService {
 		
 		if (entidade.getFuncional() == null)
 			 throw new SRHRuntimeException("O Funcionário é obrigatório. Digite o nome e efetue a pesquisa.");
+		
+		verificaTipoOcupacao(entidade.getFuncional());
 
 		if (entidade.getTipoBeneficio() == null)
 			throw new SRHRuntimeException("O Tipo Benefício é obrigatório.");
 		
 		if ( entidade.getDataAto() == null )
 			throw new SRHRuntimeException("A Data Ato é obrigatória.");
-		else if (!dataDepoisDaNomeacao(entidade.getFuncional(), entidade.getDataAto()))
+		else if (!entidade.getDataAto().after(entidade.getFuncional().getExercicio()))
 			throw new SRHRuntimeException("A Data Ato deve ser posterior a data de Exercício Funcional.");
 		
 		if (entidade.getNumeroResolucao() == null || entidade.getNumeroResolucao().intValue() <= 0)
@@ -92,34 +102,36 @@ public class AposentadoriaServiceImpl implements AposentadoriaService {
 		
 		if (entidade.getAnoResolucao() == null || entidade.getAnoResolucao().intValue() <= 0)
 			throw new SRHRuntimeException("O Ano Resolução é obrigatório.");
-		else if (anoAnteriorANomeacao(entidade.getFuncional(), entidade.getAnoResolucao()))
-			throw new SRHRuntimeException("O Ano Resolução não pode ser anterior ao ano do Exercício Funcional.");
+		else if (anoAnterior(entidade.getDataAto(), entidade.getAnoResolucao()))
+			throw new SRHRuntimeException("Ano Resolução tem que ser maior ou igual ao ano da Data Ato.");		
 		
 		if ( entidade.getDataUltimaContagem() == null )
 			throw new SRHRuntimeException("A Data Última Contagem é obrigatória.");
-		else if (!dataDepoisDaNomeacao(entidade.getFuncional(), entidade.getDataUltimaContagem()))
+		else if (!entidade.getDataUltimaContagem().after(entidade.getFuncional().getExercicio()))
 			throw new SRHRuntimeException("A Data Última Contagem deve ser posterior a data de Exercício Funcional.");
-
-		if ( entidade.getDataInicioBeneficio() != null &&		
-				!entidade.getDataInicioBeneficio().after(entidade.getDataUltimaContagem()))
+		
+		if ( entidade.getDataInicioBeneficio() == null )
+			throw new SRHRuntimeException("A Data Início Benefício é obrigatória.");
+		else if (!entidade.getDataInicioBeneficio().after(entidade.getDataUltimaContagem()))
 			throw new SRHRuntimeException("A Data Início Benefício deve ser posterior a Data Última Contagem.");
+
+		if (entidade.getTipoPublicacao() == null)
+			throw new SRHRuntimeException("O Tipo Publicação é obrigatório.");
 		
-	}
+		if ( entidade.getDataPublicacao() == null )
+			throw new SRHRuntimeException("A Data Publicação é obrigatória.");
+		else if (entidade.getDataPublicacao().before(entidade.getDataAto()))
+			throw new SRHRuntimeException("A Data Publicação não pode ser anterior a Data Ato.");		
+		
+	}	
 	
-	
-	private boolean dataDepoisDaNomeacao(Funcional funcional, Date data) {
+	private boolean anoAnterior(Date data, Long ano) {
 		
-		return data.after(funcional.getExercicio());		
-	}
-	
-	
-	private boolean anoAnteriorANomeacao(Funcional funcional, Long ano) {
-		
-		Calendar exercicio = new GregorianCalendar();
-		exercicio.setTime(funcional.getExercicio());
+		Calendar dataCalendar = new GregorianCalendar();
+		dataCalendar.setTime(data);
 		
 		
-		return ano < exercicio.get(Calendar.YEAR);		
+		return ano < dataCalendar.get(Calendar.YEAR);		
 	}
 	
 	
@@ -131,16 +143,34 @@ public class AposentadoriaServiceImpl implements AposentadoriaService {
 	}
 	
 	
-	private void atualizaFuncional(Aposentadoria entidade, Funcional funcional, boolean excluirAposentadoria) {
+	private void atualizaFuncional(Aposentadoria entidade, boolean excluirAposentadoria) {
 
+		Funcional funcional = entidade.getFuncional();
+		
 		if (excluirAposentadoria) {
 			
 			funcional.setStatus(EnumStatusFuncional.ATIVO.getId());
 			
-			if (funcional.getOcupacao().getTipoOcupacao().getId() == 1L) // Membros
+			if (funcional.getOcupacao().getTipoOcupacao().getId() != 1L) // Diferente de membros
+				funcional.setPonto(true);
+			
+			AbonoPermanencia abono = abonoPermanenciaService.getByPessoalId(entidade.getFuncional().getPessoal().getId());
+			if (abono != null && abono.getFuncional().getId().intValue() == entidade.getFuncional().getId().intValue())
+				funcional.setAbonoPrevidenciario(true);			
+						
+			
+			if (funcional.getOcupacao().getTipoOcupacao().getId() == 1L) { // Membros
+				
 				funcional.setFolha(folhaService.getByCodigo("01"));	// Conselheiros Ativos
-			else 
-				funcional.setFolha(folhaService.getByCodigo("03")); // Pessoal Ativo
+			
+			} else { 
+				
+				if (representacaoFuncionalService.temAtivaByPessoal(entidade.getFuncional().getPessoal().getId()))
+					funcional.setFolha(folhaService.getByCodigo("02")); // Chefes(cargo comissionado)
+				else
+					funcional.setFolha(folhaService.getByCodigo("03")); // Pessoal Ativo
+			
+			}			
 			
 			funcional.setAposentadoria(null);
 		
@@ -148,12 +178,30 @@ public class AposentadoriaServiceImpl implements AposentadoriaService {
 			
 			funcional.setStatus(EnumStatusFuncional.INATIVO.getId());
 			
+			funcional.setPonto(false);
+			
+			funcional.setAbonoPrevidenciario(false);			
+			
 			if (funcional.getOcupacao().getTipoOcupacao().getId() == 1L) // Membros
 				funcional.setFolha(folhaService.getByCodigo("06"));	// Conselheiros Aposentados
 			else 
 				funcional.setFolha(folhaService.getByCodigo("07")); // Servidores Aposentados
 			
-			funcional.setAposentadoria(entidade);
+						
+			FuncionalSetor lotacao = funcionalSetorService.getAtivoByFuncional(funcional.getId());			
+			
+			if(lotacao != null) {
+				if (entidade.getDataUltimaContagem().before(lotacao.getDataInicio()))
+					throw new SRHRuntimeException("A Data Última Contagem é anterior a Data Início da lotação ativa do servidor.");
+				
+				lotacao.setDataFim(entidade.getDataUltimaContagem());
+				funcionalSetorService.salvar(lotacao);
+			}		
+			
+			funcional.setSetor(setorService.getById(113L));		
+			
+			
+			funcional.setAposentadoria(entidade);			
 			
 		}		
 		
