b4bcdc97ebda4f076815bbeec19fd1df306d2d39
TCE\felipe.augusto <felipe.augusto@tce.ce.gov.br>
2019-03-07 14:53:25 -0300

    Foi adicionado um filtro de servidor no relatório de férias. Ao escolher um servidor, o relatório em PDF mostra a coluna observação dos registros de férias filtrados.

--- srh/src/main/java/br/gov/ce/tce/srh/dao/RelatorioFeriasDAO.java
+++ srh/src/main/java/br/gov/ce/tce/srh/dao/RelatorioFeriasDAO.java
@@ -3,11 +3,12 @@ package br.gov.ce.tce.srh.dao;
 import java.util.Date;
 import java.util.List;
 
+import br.gov.ce.tce.srh.domain.Funcional;
 import br.gov.ce.tce.srh.domain.RelatorioFerias;
 import br.gov.ce.tce.srh.domain.TipoOcupacao;
 import br.gov.ce.tce.srh.sapjava.domain.Setor;
 
 public interface RelatorioFeriasDAO {
-	public abstract int getCountFindByParameter(Setor setor, List<String> tiposFerias, Date inicio, Date fim, Long anoReferencia, TipoOcupacao tipoOcupacao);
-	public abstract List<RelatorioFerias> findByParameter(Setor setor, List<String> tiposFerias, Date inicio, Date fim, Long anoReferencia, TipoOcupacao tipoOcupacao, int firstResult, int maxResults);
+	public abstract int getCountFindByParameter(Funcional funcional, Setor setor, List<String> tiposFerias, Date inicio, Date fim, Long anoReferencia, TipoOcupacao tipoOcupacao);
+	public abstract List<RelatorioFerias> findByParameter(Funcional funcional, Setor setor, List<String> tiposFerias, Date inicio, Date fim, Long anoReferencia, TipoOcupacao tipoOcupacao, int firstResult, int maxResults);
 }
