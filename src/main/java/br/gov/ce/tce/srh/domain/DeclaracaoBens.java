620ce020cce470ab469d975af08296f33e47881d
TCE\felipe.augusto <felipe.augusto@tce.ce.gov.br>
2020-11-12 10:55:15 -0300

    incluindo regras de obrigatoriedade no cadastro da declaração

--- src/main/java/br/gov/ce/tce/srh/domain/DeclaracaoBens.java
+++ src/main/java/br/gov/ce/tce/srh/domain/DeclaracaoBens.java
@@ -92,6 +92,7 @@ public class DeclaracaoBens extends BasicEntity<Long> implements Serializable {
 	public int hashCode() {
 		final int prime = 31;
 		int result = super.hashCode();
+		result = prime * result + ((caminho == null) ? 0 : caminho.hashCode());
 		result = prime * result + ((id == null) ? 0 : id.hashCode());
 		return result;
 	}
@@ -105,12 +106,17 @@ public class DeclaracaoBens extends BasicEntity<Long> implements Serializable {
 		if (getClass() != obj.getClass())
 			return false;
 		DeclaracaoBens other = (DeclaracaoBens) obj;
+		if (caminho == null) {
+			if (other.caminho != null)
+				return false;
+		} else if (!caminho.equals(other.caminho))
+			return false;
 		if (id == null) {
 			if (other.id != null)
 				return false;
 		} else if (!id.equals(other.id))
 			return false;
 		return true;
-	}	
+	}		
 
 }
