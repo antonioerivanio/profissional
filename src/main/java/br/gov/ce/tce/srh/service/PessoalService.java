08ec737d359954f8a135c7511d953eb674f90173
eduardo9200 <edumaia92@gmail.com>
2021-11-19 15:07:31 -0300

    Remoção temporária dos campos, "é readaptado" e "entrou por cotas" e suas validações, já que são campos obrigatórios.

--- src/main/java/br/gov/ce/tce/srh/service/PessoalService.java
+++ src/main/java/br/gov/ce/tce/srh/service/PessoalService.java
@@ -157,11 +157,11 @@ public class PessoalService {
 			if( entidade.getTipoDeficiencia() == null )
 				throw new SRHRuntimeException("O Tipo Deficiência é obrigatório.");
 			
-			if(entidade.getIngressouPorCotas() == null || entidade.getIngressouPorCotas().isEmpty())
-				throw new SRHRuntimeException("É necessário informar se você entrou por cotas.");
+			/*if(entidade.getIngressouPorCotas() == null || entidade.getIngressouPorCotas().isEmpty())
+				throw new SRHRuntimeException("É necessário informar se você entrou por cotas.");*/
 			
-			if(entidade.getIsReadaptado() == null || entidade.getIsReadaptado().isEmpty())
-				throw new SRHRuntimeException("É necessário informar se você é readaptado.");
+			/*if(entidade.getIsReadaptado() == null || entidade.getIsReadaptado().isEmpty())
+				throw new SRHRuntimeException("É necessário informar se você é readaptado.");*/
 			
 			if ( entidade.getRaca() == null )
 				throw new SRHRuntimeException("A Raça/Cor é obrigatória.");
